5fff59fd00a6d9d67027d27b7378d7acb3d41a32
Ilnytskyy Markiian <ilnytskyymarkiian@gmail.com>
2026-04-29 16:26:30 +0300

    feat: add input validation for create-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,31 +13,55 @@ import {
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+export type State = {
+    errors?: {
+        customerId?: string[],
+        amount?: string[],
+        status?: string[],
+    }  
+    message?: string | null,
+};
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(), // coerce – type coercion (implicit type convertion. Like "5 + '9' === '59'")
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    // coerce – type coercion (implicit type convertion. Like "5 + '9' === '59'")
+    amount: z.coerce
+        .number()
+        .gt(0, 'Please enter an amount greater tahn $0.'),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    try {
-        const { customerId, amount, status } = CreateInvoice.parse({
-            customerId: formData.get('customerId'),
-            amount: formData.get('amount'),
-            status: formData.get('status'),
-        });
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
 
-        // Good practice to convert money to cents to eliminate floating-type inpercisions
-        const amountInCents = amount * 100;
-        const date = new Date().toISOString().split('T')[0];
-
-        console.log(date);
+    if(!validatedFields.success) {
+        console.log(validatedFields);
         
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        }
+    }
+
+    // Good practice to convert money to cents to eliminate floating-type inpercisions
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    
+    try {        
         await sql`
             INSERT
                 INTO invoices(customer_id, amount, status, date)
@@ -56,15 +80,15 @@ export async function createInvoice(formData: FormData) {
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+    
+  const amountInCents = amount * 100;
+  
   try {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    
-    const amountInCents = amount * 100;
-    
     await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
